f6c81e3365e78bd7564884b41feb5f7017dc0de2
KyleJewiss <kyle.jewiss@gmail.com>
2024-08-11 20:03:10 +1000

    🚧 feat: added a dumb win check method

--- main.cpp
+++ main.cpp
@@ -49,10 +49,8 @@ Y
 using namespace std;
 
 const int8_t BOARD_SIZE = 3;
-vector<vector<vector<char>>> board(BOARD_SIZE, vector<vector<char>>(BOARD_SIZE, vector<char>(BOARD_SIZE, ' ')));
-array<int, BOARD_SIZE> player_move;
 
-void print_board()
+void print_board(vector<vector<vector<char>>> &board)
 {
     cout << "\n";
     cout << "          /-----------/|\n";
@@ -81,7 +79,7 @@ void print_board()
     cout << "\n";
 }
 
-bool is_valid_move(array<int, BOARD_SIZE> &move)
+bool is_valid_move(vector<vector<vector<char>>> &board, array<int, BOARD_SIZE> &move)
 {
 
     if (move[0] < 0 || move[0] >= BOARD_SIZE || move[1] < 0 || move[1] >= BOARD_SIZE || move[2] < 0 || move[2] >= BOARD_SIZE)
@@ -101,7 +99,7 @@ bool is_valid_move(array<int, BOARD_SIZE> &move)
     }
 }
 
-array<int, BOARD_SIZE> get_move()
+array<int, BOARD_SIZE> get_move(vector<vector<vector<char>>> &board, array<int, BOARD_SIZE> &player_move)
 {
     while (true)
     {
@@ -112,7 +110,7 @@ array<int, BOARD_SIZE> get_move()
 
         if (ss >> player_move[0] >> player_move[1] >> player_move[2]) // Ensure the input is 3 numbers spaced apart
         {
-            if (ss.eof() && is_valid_move(player_move))
+            if (ss.eof() && is_valid_move(board, player_move))
             {
                 return player_move;
             }
@@ -122,20 +120,106 @@ array<int, BOARD_SIZE> get_move()
     }
 }
 
+bool check_win(vector<vector<vector<char>>> &board, char &player_character)
+{
+    for (int i = 0; i < BOARD_SIZE; i++)
+    {
+        // Check rows
+        if (board[i][0][0] == player_character && board[i][1][0] == player_character && board[i][2][0] == player_character)
+        {
+            return true;
+        }
+        else if (board[i][0][1] == player_character && board[i][1][1] == player_character && board[i][2][1] == player_character)
+        {
+            return true;
+        }
+        else if (board[i][0][2] == player_character && board[i][1][2] == player_character && board[i][2][2] == player_character)
+        {
+            return true;
+        }
+
+        // Check columns
+        else if (board[0][i][0] == player_character && board[1][i][0] == player_character && board[2][i][0] == player_character)
+        {
+            return true;
+        }
+        else if (board[0][i][1] == player_character && board[1][i][1] == player_character && board[2][i][1] == player_character)
+        {
+            return true;
+        }
+        else if (board[0][i][2] == player_character && board[1][i][2] == player_character && board[2][i][2] == player_character)
+        {
+            return true;
+        }
+
+        // Check diagonals
+        else if (board[0][0][i] == player_character && board[1][1][i] == player_character && board[2][2][i] == player_character)
+        {
+            return true;
+        }
+        else if (board[0][2][i] == player_character && board[1][1][i] == player_character && board[2][0][i] == player_character)
+        {
+            return true;
+        }
+
+        // Check verticals
+        else if (board[0][i][0] == player_character && board[0][i][1] == player_character && board[0][i][2] == player_character)
+        {
+            return true;
+        }
+        else if (board[1][i][0] == player_character && board[1][i][1] == player_character && board[1][i][2] == player_character)
+        {
+            return true;
+        }
+        else if (board[2][i][0] == player_character && board[2][i][1] == player_character && board[2][i][2] == player_character)
+        {
+            return true;
+        }
+
+        // Check vertical diagonals
+        else if (board[0][0][0] == player_character && board[1][1][1] == player_character && board[2][2][2] == player_character)
+        {
+            return true;
+        }
+        else if (board[2][2][0] == player_character && board[1][1][1] == player_character && board[0][0][2] == player_character)
+        {
+            return true;
+        }
+        else if (board[0][2][0] == player_character && board[1][1][1] == player_character && board[2][0][2] == player_character)
+        {
+            return true;
+        }
+        else if (board[2][0][0] == player_character && board[1][1][1] == player_character && board[0][2][2] == player_character)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 int main()
 {
-    print_board();
+    vector<vector<vector<char>>> board(BOARD_SIZE, vector<vector<char>>(BOARD_SIZE, vector<char>(BOARD_SIZE, ' ')));
+    array<int, BOARD_SIZE> player_move;
+
+    print_board(board);
     cout << "Example move: 0 0 0\n";
+
     // Game loop
     for (int i = 0; i < BOARD_SIZE ^ 3; i++)
     {
-        player_move = get_move();
-
+        player_move = get_move(board, player_move);
         char player_character = (i % 2 == 0) ? 'X' : 'O';
-
         board[player_move[0]][player_move[1]][player_move[2]] = player_character;
 
-        print_board();
+        print_board(board);
+
+        if (check_win(board, player_character))
+        {
+            cout << "Player " << player_character << " wins!\n";
+            break;
+        }
     }
 
     return 0;
